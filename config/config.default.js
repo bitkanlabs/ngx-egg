0f46505465e23f44a2f29144af525f725870698b
Clark Wang <clark@bitkan.com>
2018-08-26 01:32:00 +0800

    initial commit

---
+++ config/config.default.js
@@ -0,0 +1,28 @@
+'use strict';
+const path = require('path');
+
+module.exports = appInfo => {
+  const config = {};
+
+  config.middleware = [];
+
+  config.static = {
+    dir: path.join(appInfo.baseDir, 'app/browser'),
+    prefix: '/',
+  };
+
+  config.view = {
+    root: path.join(appInfo.baseDir, 'app/browser'),
+    mapping: {
+      '.html': 'angular',
+    },
+    defaultExtension: '.html',
+    defaultViewEngine: 'angular',
+  };
+
+  config.angular = {
+    main: path.join(appInfo.baseDir, 'app/server/main'),
+  };
+
+  return config;
+};
---
+++ config/plugin.js
@@ -0,0 +1,7 @@
+'use strict';
+
+// add you build-in plugin here, example:
+exports.angular = {
+  enable: true,
+  package: 'egg-view-angular',
+};
